0cd2c0dc21862c2cb7b9aef5d35a84ddfdb8a0af
SurajK2025 <suraj.khokrale@rediffmail.com>
2023-03-01 11:34:16 +0530

    Updated Profile Page

--- frontend/src/components/ProfileSettings.js
+++ frontend/src/components/ProfileSettings.js
@@ -12,6 +12,29 @@ const ProfileSettings = (props) => {
                 </ol>
             </div>
             <div class="settingsContainer">
+                <form action="">
+                    <h3>Update Password</h3>
+                    <table>
+                        <tr>
+                            <td><label for="">Old Password</label></td>
+                            <td><input type="password" name="pass" id="" placeholder="*******" required /><br /></td>
+                        </tr>
+
+                        <tr>
+                            <td><label for="">New Password</label></td>
+                            <td><input type="password" name="pass" id="" placeholder="*******" required /><br /></td>
+                        </tr>
+
+                        <tr>
+                            <td><label for="">ReType New Password</label></td>
+                            <td><input type="password" name="pass" id="" placeholder="*******" required /><br /></td>
+                        </tr>
+
+                        <tr>
+                            <td><input type="submit" value="Edit" /> <br /></td>
+                        </tr>
+                    </table>
+                </form>
             </div>
         </div >
     );
